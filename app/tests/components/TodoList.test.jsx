05378901202d5cf89510275a9304bce3d459212f
Sam Thomson <samt@samt.st>
2017-10-16 21:36:45 +0100

    testing++

---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,40 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var TestUtils = require('react-addons-test-utils')
+var expect = require('expect')
+var $ = require('jquery')
+
+var TodoList = require('TodoList')
+var Todo = require('Todo')
+
+describe('TodoList', () => {
+    it('should exist', () => {
+        expect(TodoList).toExist()
+    })
+
+    it('should render a todo for each todo item', () => {
+
+        var todos = [
+            {
+                id: 1,
+                text: 'Go to bed'
+            },
+            {
+                id: 2,
+                text: 'finish react course'
+            },
+            {
+                id: 3,
+                text: 'graph ql course'
+            },
+            {
+                id: 4,
+                text: '500px some pics'
+            }
+        ]
+        var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />)
+        var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo)
+
+        expect(todosComponents.length).toEqual(todos.length)
+    })
+})
