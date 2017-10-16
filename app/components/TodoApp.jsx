5aeba71fa5ff89f1d47537e8ad10f9adfff0879f
Sam Thomson <samt@samt.st>
2017-10-16 21:19:29 +0100

    started adding components

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,11 +1,35 @@
 var React = require('react')
-
+var TodoList = require('TodoList')
 
 var TodoApp = React.createClass({
+    getInitialState: function () {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    text: 'Go to bed'
+                },
+                {
+                    id: 2,
+                    text: 'finish react course'
+                },
+                {
+                    id: 3,
+                    text: 'graph ql course'
+                },
+                {
+                    id: 4,
+                    text: '500px some pics'
+                }
+            ]
+        }
+    },
     render: function () {
+        var { todos } = this.state
+
         return (
             <div>
-                TodoApp.jsx
+                <TodoList todos={todos} />
             </div>
         )
     }
