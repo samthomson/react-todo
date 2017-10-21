02472c55e29f9f084c1f7afa6868f24666342dbb
Sam Thomson <samt@samt.st>
2017-10-21 20:38:00 +0100

    more redux into todo app

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,17 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp')
 
+var actions = require('actions')
+var store = require('configureStore').configure()
+
+store.subscribe(() => {
+  console.log('New state', store.getState())
+})
+
+store.dispatch(actions.addTodo('get dinner'))
+store.dispatch(actions.setSearchText('add a todo'))
+store.dispatch(actions.toggleShowCompleted())
+
 // Load foundation
 $(document).foundation();
 
---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,63 @@
+var expect = require('expect')
+var df = require('deep-freeze-strict')
+
+var reducers = require('reducers')
+
+
+describe('reducers', () => {
+    describe('searchText', () => {
+        it('should set searchText', () => {
+            var action = {
+                type: 'SET_SEARCH_TEXT',
+                searchText: 'dog'
+            }
+            var res = reducers.searchTextReducer(df(''), df(action))
+
+            expect(res).toEqual(action.searchText)
+        })
+    })
+
+    describe('showCompletedReducer', () => {
+        it('should toggle show completed status', () => {
+            var action = {
+                type: 'TOGGLE_SHOW_COMPLETED'
+            }
+            var res = reducers.showCompletedReducer(df(false), df(action))
+
+            expect(res).toEqual(true)
+        })
+    })
+
+    describe('todosReducer', () => {
+        it('should add new todo', () => {
+            var action = {
+                type: 'ADD_TODO',
+                text: 'something to do'
+            }
+            var res = reducers.todosReducer(df([]), df(action))
+
+            expect(res.length).toEqual(1)
+            expect(res[0].text).toEqual(action.text)
+        })
+
+        it('should toggle todo', () => {
+            var todos = [
+                {
+                    id: 11,
+                    text: 'test features',
+                    completed: false,
+                    createdAt: 2433432,
+                    completedAt: undefined
+                }
+            ]
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: 11
+            }
+            var res = reducers.todosReducer(df(todos), df(action))
+
+            expect(res[0].completed).toEqual(true)
+            expect(res[0].completedAt).toEqual(undefined)
+        })
+    })
+})
